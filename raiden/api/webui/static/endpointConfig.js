0f305f5e63c76fa38d1fbf4446899c5346952749
Marcos <martinez.marcos@outloook.com>
2019-05-12 20:04:51 -0300

    RNS update

--- raiden/api/webui/static/endpointConfig.js
+++ raiden/api/webui/static/endpointConfig.js
@@ -1,6 +1,6 @@
 const backendUrl='http://localhost:5001'; 
 const nodeAddress = '0x68EC2b76925e3d2ece474ED5c39a11DbA16C5BAF'; 
-const rnsDomain = 'marcos.rsk.co';
+const rnsDomain = null 
 const chainEndpoint = 'http://127.0.0.1:4444'; 
 
 window.luminoUrl = backendUrl;
